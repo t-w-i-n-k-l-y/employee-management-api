0ca9449215323375b9c724b890cd993e16875b3a
t-w-i-n-k-l-y <hkalani00@gmail.com>
2025-02-16 03:00:37 +0530

    Add custom exception DuplicateValueException and update Global exception handler

--- src/main/java/com/example/employee_management_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/employee_management_api/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.employee_management_api.exception;
 
+import com.example.employee_management_api.util.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,12 +10,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<APIResponse<String>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(e.getMessage(), null, 400));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGeneralException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong!");
+    public ResponseEntity<APIResponse<String>> handleGeneralException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new APIResponse<>("Something went wrong!", null, 500));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<APIResponse<String>> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new APIResponse<>("Unexpected error occurred!", null, 500));
+    }
+
+    @ExceptionHandler(DuplicateValueException.class)
+    public  ResponseEntity<APIResponse<String>> handleDuplicateValueException(DuplicateValueException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(e.getMessage(), null, 400));
     }
 }
