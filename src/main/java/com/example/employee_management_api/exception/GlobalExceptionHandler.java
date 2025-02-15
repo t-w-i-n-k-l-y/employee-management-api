6a1ad9d5315d2ca44783e25dce7e0c94b8b5edef
t-w-i-n-k-l-y <hkalani00@gmail.com>
2025-02-16 00:11:27 +0530

    Add DTO and mapper with global exception handler

---
+++ src/main/java/com/example/employee_management_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.employee_management_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong!");
+    }
+}
